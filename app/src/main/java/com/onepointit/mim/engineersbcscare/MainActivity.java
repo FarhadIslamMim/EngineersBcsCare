baa2592ce0b0ab30ac22600112ed908618387ce7
mahide41 <mahide41@gmail.com>
2023-02-06 14:41:28 +0600

    "update"

--- app/src/main/java/com/onepointit/mim/engineersbcscare/MainActivity.java
+++ app/src/main/java/com/onepointit/mim/engineersbcscare/MainActivity.java
@@ -5,8 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.onepointit.mim.engineersbcscare.ui.bank_routine.BankRoutineFragment;
@@ -29,6 +33,34 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
         bottomNavigationView.setOnNavigationItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.navigation_dashboard);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater =getMenuInflater();
+        inflater.inflate(R.menu.menu,menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_logout) {
+            // do something here
+            Toast.makeText(this, "Logout clicked", Toast.LENGTH_SHORT).show();
+            //Intent intent = new Intent(this, MainMenuActivity.class);
+            //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            //startActivity(intent);
+        }
+        if (id == R.id.action_menu) {
+            Toast.makeText(this, "Menu Clicked", Toast.LENGTH_SHORT).show();
+            //Intent intent = new Intent(this, Notification.class);
+            //intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            //startActivity(intent);
+            // finish();
+        }
+        return true;
+    }
+
+
 
     DashboardFragment dashboardFragment = new DashboardFragment();
     BcsRoutineFragment bcsRoutineFragment = new BcsRoutineFragment();
